383fb057fb2411cf9a682df3d244643c5b403c2b
Will Ávila <willaviladev@gmail.com>
2022-05-19 19:27:25 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import { GridItem } from './components/GridItem';
+
+import { levels, calculateImc, Level } from './helpers/imc';
+
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
+
+  const handleCalculateButton = () => {
+    if(heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
+    } else {
+      alert("Digite todos os campos.")
+    }
+  }
+
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+          <p>IMC é a sigla para indice de massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso idaeal de cada pessoa.</p>
+
+          <input
+            type="number"
+            placeholder='Digite a sua altura. Ex 1.5 (em metros)'
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+          
+          <input
+            type="number"
+            placeholder='Digite o seu peso. Ex 75.3 (em Kg)'
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+          <button onClick={handleCalculateButton}>CALCULAR</button>
+        </div>
+        <div className={styles.rightSide}>
+          {!toShow &&
+          <div className={styles.grid}>
+            {levels.map((item, key)=> (
+              <GridItem key={key} item={item} />
+            ))}
+          </div>
+          }
+          {toShow &&
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow}></div>
+              <GridItem item={toShow} />
+            </div>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
